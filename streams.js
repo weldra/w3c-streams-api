1b0857c0fbfa75a82659ae0ca4462d3cf3c4a53f
Weldra <public@weldra.org>
2013-06-13 20:45:54 +0300

    Update streams.js

--- streams.js
+++ streams.js
@@ -52,16 +52,77 @@ self.StreamReader = self.StreamReader || function() {
     this.readyState = 0;
   }
 
+  /**
+   *
+   * @link http://www.w3.org/TR/streams-api/#widl-StreamReader-readAsBlob-void-Stream-stream-unsigned-long-long-maxSize
+   */
   function readAsBlob(stream, maxSize) {
+    // If maxSize is less than one, through an Invalid Argument exception. Terminate these overall steps.
+    if (maxSize < 1) {
+      throw new Error("Invalid Argument");
+    }
+    // If readyState is LOADING, raise a NOT_ALLOWED_ERR exception and terminate these steps.
+    if (this.readyState === StreamReader.LOADING) {
+      throw new Error("NOT_ALLOWED_ERR");
+    }
+    
+    // If an error occurs during reading the stream parameter, set readyState to DONE and set result to null. Proceed to the error steps below.
+    
+    // If no error has occurred, set readyState to LOADING.
+    this.readyState = StreamReader.LOADING;
+    
+    // Fire a progress event called loadstart.
     if (this.onloadstart) {
       this.onloadstart();
     }
+    
+    // Make progress notifications.
+    
+    // Continue reading on the Stream:
+    // If the optional maxSize parameter has been set, set the readyState to DONE when the number of bytes read reaches MAX_SIZE or the stream has been fully read and the number of bytes is less than MAX_SIZE.
+    if (maxSize) {
+      
+    }
+    // If the optional parameter has not been set, set readyState to DONE when the stream has been fully read.
+    else {
+      
+    }
+    
+    // Set the result attribute to be stream’s data content represented as a Blob; on getting, the result result returns the (complete) data of stream as a Blob.
+    this.result = new Blob(data, { type: stream.type });
+
+    // Terminate this overall set of steps.
   }
 
+  /**
+   *
+   * @link http://www.w3.org/TR/streams-api/#widl-StreamReader-readAsArrayBuffer-void-Stream-stream-unsigned-long-long-maxSize
+   * @link http://www.w3.org/TR/FileAPI/#readAsArrayBuffer
+   */
   function readAsArrayBuffer(stream, maxSize) {
+    // If maxSize is less than one, throw an Invalid Argument exception. Terminate these overall steps.
+    if (maxSize < 1) {
+      throw new Error("Invalid Argument");
+    }
+    // If readyState = LOADING throw an InvalidStateError exception [DOM4] and terminate these steps.
+    if (this.readyState === StreamReader.LOADING) {
+      throw new Error("InvalidStateError");
+    }
+    // If the blob has been neutered through the close method, throw an InvalidStateError exception [DOM4] and terminate this algorithm.
+    if (stream._closed) {
+      throw new Error("InvalidStateError");
+    }
+    
+    // If an error occurs during reading the blob parameter, set readyState to DONE and set result to null. Proceed to the error steps.
+    // If no error has occurred, set readyState to LOADING
+    this.readyState = StreamReader.LOADING;
+    
+    // Fire a progress event called loadstart.
     if (this.onloadstart) {
       this.onloadstart();
     }
+    
+    // Return the readAsArrayBuffer() method, but continue to process the steps in this algorithm.
   }
 
   function readAsText(stream, encoding, maxSize) {
@@ -84,6 +145,11 @@ self.StreamReader = self.StreamReader || function() {
     }
   }
 
+  /**
+   *
+   * @link http://www.w3.org/TR/streams-api/#widl-StreamReader-abort-void
+   * @link http://www.w3.org/TR/FileAPI/#abort
+   */
   function abort() {
     // If readyState = EMPTY or if readyState = DONE set result to null and terminate this overall set of steps without doing anything else.
     if (this.readyState === StreamReader.EMPTY || this.readyState === StreamReader.DONE) {
@@ -98,11 +164,11 @@ self.StreamReader = self.StreamReader || function() {
     
     // Terminate the algorithm for the read method being processed.
     
-    // Fire a progress event called abort
+    // Fire a progress event called abort.
     if (this.onabort) {
       this.onabort();
     }
-    // Unless readyState is LOADING fire a progress event called loadend. If readyState is LOADING do NOT fire loadend.
+    // Fire a progress event called loadend.
     if (this.readyState !== StreamReader.LOADING) {
       if (this.onloadend) {
         this.onloadend();
